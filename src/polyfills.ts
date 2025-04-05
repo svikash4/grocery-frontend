af94f322a5946a76654078c7ef30750f403aead6
svikash4 <vikash.sharma749@gmail.com>
2025-04-05 22:52:34 +0530

    Adding files

---
+++ src/polyfills.ts
@@ -0,0 +1,5 @@
+/***************************************************************************************************
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills here.
+ */
+import 'zone.js'; 
