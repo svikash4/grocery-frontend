2616916d6013f771fbdf7fbd4852116263a21699
svikash4 <vikash.sharma749@gmail.com>
2025-04-05 22:09:33 +0530

    Adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',   // 👈 This must match index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
