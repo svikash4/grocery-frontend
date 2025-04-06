11e49f2e515ba95c14a30ddff4c90f54fc3165f5
svikash4 <vikash.sharma749@gmail.com>
2025-04-06 13:32:22 +0530

    Adding files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,18 @@ import { CartComponent } from './pages/cart/cart.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
+import { AuthGuard } from './guards/auth.guard'; // add this line
+
 const routes: Routes = [
-    { path: '', pathMatch: 'full', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'cart', component: CartComponent },
-    { path: 'orders', component: OrdersComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: '**', redirectTo: '' }
-  ];
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
+  { path: 'orders', component: OrdersComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
